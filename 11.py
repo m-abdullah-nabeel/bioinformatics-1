44ea3c68b8aa3c94cb92c58a49833bd0f9e7b264
m-abdullah-nabeel <muhammadabdullahnabeel@gmail.com>
2023-08-15 01:14:10 +0500

    week 2 completed

--- 11.py
+++ 11.py
@@ -1,14 +1,46 @@
-# def Neighbors(Pattern, d):
-#     if d == 0:
-#         return {Pattern}
-#     if len(Pattern) == 1:
-#         return {'A', 'C', 'G', 'T'}
-#     Neighborhood = ()
-#     SuffixNeighbors ← Neighbors(Suffix(Pattern), d)
-#     for each string Text from SuffixNeighbors
-#         if HammingDistance(Suffix(Pattern), Text) < d
-#             for each nucleotide x
-#                 add x • Text to Neighborhood
-#         else
-#             add FirstSymbol(Pattern) • Text to Neighborhood
-#     return Neighborhood
+def HammingDistance(a, b):
+    a = a.lower()
+    b = b.lower()
+    hd = 0
+    for i in range(len(a)):
+        if a[i] != b[i]:
+            hd += 1
+    return(hd)
+
+
+def Suffix(Pattern): 
+    return Pattern[1:]
+
+def Neighbors(Pattern, d): 
+    if d == 0: 
+        return {Pattern}
+    if len(Pattern) == 1: 
+        return {'A', 'C', 'G', 'T'}
+    Neighborhood = set()
+    SuffixNeighbors = Neighbors(Suffix(Pattern), d)
+    # for each string Text from SuffixNeighbors:
+    for Text in SuffixNeighbors:
+        if HammingDistance(Suffix(Pattern), Text) < d:
+            # for each nucleotide x:
+            for x in 'ATCG':
+                # add x • Text to Neighborhood
+                Neighborhood.add(x+Text)
+        else:
+            # add FirstSymbol(Pattern) • Text to Neighborhood
+            Neighborhood.add(Pattern[0]+Text)
+
+    return Neighborhood
+
+
+samp = 'CATCACGTGC'
+# x = Suffix(samp)
+# print(x)
+
+# z = Neighbors('ACG', 1)
+z = Neighbors(samp, 2)
+print(z)
+
+for i in list(z): 
+    print(i)
+
+print(len(list(z)))
---
+++ 12.py
@@ -0,0 +1,74 @@
+def revers_compliment(DNA):
+    reverse = DNA[::-1]
+    print(f"{reverse} = Reverse")
+    compliment = ''
+    for b in reverse:
+        if b == 'A': compliment = compliment + 'T'
+        if b == 'T': compliment = compliment + 'A'
+        if b == 'C': compliment = compliment + 'G'
+        if b == 'G': compliment = compliment + 'C'
+    print(f"{compliment} = Reverse Compliment")
+    return compliment
+
+
+def HammingDistance(a, b):
+    a = a.lower()
+    b = b.lower()
+    hd = 0
+    for i in range(len(a)):
+        if a[i] != b[i]:
+            hd += 1
+    return(hd)
+
+
+def Suffix(Pattern): 
+    return Pattern[1:]
+
+
+def Neighbors(Pattern, d): 
+    if d == 0: 
+        return {Pattern}
+    if len(Pattern) == 1: 
+        return {'A', 'C', 'G', 'T'}
+    Neighborhood = set()
+    SuffixNeighbors = Neighbors(Suffix(Pattern), d)
+    # for each string Text from SuffixNeighbors:
+    for Text in SuffixNeighbors:
+        if HammingDistance(Suffix(Pattern), Text) < d:
+            # for each nucleotide x:
+            for x in 'ATCG':
+                # add x • Text to Neighborhood
+                Neighborhood.add(x+Text)
+        else:
+            # add FirstSymbol(Pattern) • Text to Neighborhood
+            Neighborhood.add(Pattern[0]+Text)
+
+    return Neighborhood
+
+
+def FrequentWordsWithMismatches(Text, k, d): 
+    Patterns = []
+    freqMap ={}
+    n = len(Text)
+    for i in range(1+n - k): #check if any error
+        Pattern = Text[i: i+k] 
+        # RevComp = revers_compliment(Pattern)
+        neighborhood = list(Neighbors(Pattern, d))
+        for j in range(len(neighborhood)-1): #check if any error
+            neighbor = neighborhood[j]
+            if neighbor in freqMap:
+                freqMap[neighbor] = freqMap[neighbor] + 1
+            else:
+                freqMap[neighbor] = 1
+
+    m = max(freqMap.values())
+    for key in freqMap:
+        if freqMap[key] == m:
+            Patterns.append(key)
+            # append Pattern to Patterns
+    return Patterns
+
+
+sample = "CTCCCGCTCGCTTTAATTAATTAAAAGACGCTAAGACTCCCGCTTTAAAAGACTCCCGCTCTCCCTCCTTAATCACGCTAAGACGCTAAGATTAATCACTCCTTAATTAACGCTAAGATTAACTCCTCACTCCTTAATTAACGCTCTCCTTAATCAAAGACTCCTCACGCTCTCCTTAACGCTTTAACGCTTCATCATCATCATCACGCTTTAACTCCAAGACGCTAAGACGCTTCAAAGATCATCACTCCAAGAAAGACGCTAAGATCATCATCACGCTCTCCCGCTTCATTAACTCCCTCCTCACGCTTTAACTCCCTCCCGCTCTCCTCAAAGATCACGCTTCAAAGAAAGACGCTTTAATCATTAACGCTTCATCAAAGACTCCAAGACTCC"
+res = FrequentWordsWithMismatches(sample, 7, 2)
+print(res)
